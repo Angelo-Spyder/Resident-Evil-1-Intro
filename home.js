948a17930053856721bd76916e089f360d102287
Angelo Gabriel <Angello.Dev.42@gmail.com>
2022-04-09 16:23:20 -0300

    Criando css na seleção, adicionando header igual e consertando href no js

--- home.js
+++ home.js
@@ -4,4 +4,9 @@ var audioTitle = document.querySelector(".title-voice");
 
 botaoIniciar.addEventListener("click",function(){
     audioTitle.play();
+    setTimeout(
+        function(){
+            location.href = "seleção/selecao.html"
+        }, 3000 );
+
 });
