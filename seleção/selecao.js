16fbaf21a797d639cd5ae4d6ed16f0993fcb8d11
Angelo Gabriel <Angello.Dev.42@gmail.com>
2022-04-09 17:13:55 -0300

    Criando a versão do Chris de seleção

--- seleção/selecao.js
+++ seleção/selecao.js
@@ -1,6 +1,11 @@
 
 var botaoJill = document.querySelector("#Jill");
+var botaoChris = document.querySelector("#Chris");
 
 botaoJill.addEventListener("click", function(){
     location.href = "../Jill/Jill.html"
+});
+
+botaoChris.addEventListener("click", function(){
+    location.href = "../Chris/Chris.html"
 });
